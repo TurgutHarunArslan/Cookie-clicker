414226941ed9802ce691f57b0aa9213bc379bcf1
Turgut Harun Arslan <83730616+TurgutHarunArslan@users.noreply.github.com>
2022-06-23 12:23:00 +0300

    Update
    Cookie clicker Multipler

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 var score = 0;
+var multi = 1;
 var cursorcost = 15;
 var cursors = 0;
 var grandmacost = 100;
 var grandmas = 0;
+var multicost = 10;
 
-function skorekle(amount){
-score = score + amount;
+function skorekle(){
+score += 1 * multi;
 document.getElementById("score").innerHTML = score;
 playAudio();
 
+}
+
+function buymult(){
+if(score >= multicost){
+score = score - multicost;
+multi = multi + 1;
+multicost = Math.round(multicost * 5);
+
+document.getElementById("score").innerHTML = score;
+document.getElementById("multcost").innerHTML = multicost;
+document.getElementById("multi").innerHTML = multi;
+} else{
+    alert("Yeterli Kurabiyen Yok");
+    }
 }
 
 function buycursor(){
@@ -32,7 +48,7 @@ document.getElementById("cursors").innerHTML = cursors;
         if(score >= grandmacost) {
         score = score - grandmacost;
         grandmas = grandmas + 1;
-        grandmacost = Math.round(grandmacost * 1.30)
+        grandmacost = Math.round(grandmacost * 1.30);
         
         document.getElementById("score").innerHTML = score;
         document.getElementById("grandmacost").innerHTML = grandmacost;
@@ -43,18 +59,20 @@ document.getElementById("cursors").innerHTML = cursors;
         }
         
         setInterval(function(){
-            score = score + cursors;
-            score = score + grandmas * 3;
+            score = score + cursors * multi;
+            score = score + grandmas * 3 * multi;
             document.getElementById("score").innerHTML = score;
             }, 2000);
 
             function loadgame() {
-            var savedgame = JSON.parse(localStorage.getItem("gamesave"))
+            var savedgame = JSON.parse(localStorage.getItem("gamesave"));
             if (typeof savedgame.score !== "undefined") score =savedgame.score;
             if (typeof savedgame.cursors !== "undefined") cursors =savedgame.cursors;
             if (typeof savedgame.cursorcost !== "undefined") cursorcost =savedgame.cursorcost;
             if (typeof savedgame.grandmas !== "undefined") grandmas =savedgame.grandmas;
             if (typeof savedgame.grandmacost !== "undefined") grandmacost =savedgame.grandmacost;
+            if (typeof savedgame.multi !== "undefined") multi =savedgame.multi;
+            if (typeof savedgame.multicost !== "undefined") multicost =savedgame.multicost;
             }
 
 
@@ -66,7 +84,9 @@ document.getElementById("cursors").innerHTML = cursors;
             cursors: cursors,
             cursorcost: cursorcost,
             grandmas: grandmas,
-            grandmacost: grandmacost
+            grandmacost: grandmacost,
+            multi: multi,
+            multicost: multicost
             };
             localStorage.setItem("gamesave",JSON.stringify(gamesave))
             }
@@ -91,6 +111,8 @@ document.getElementById("cursors").innerHTML = cursors;
             document.getElementById("cursors").innerHTML = cursors;
             document.getElementById("grandmacost").innerHTML = grandmacost;
             document.getElementById("grandmas").innerHTML = grandmas;
+            document.getElementById("multi").innerHTML = multi;
+            document.getElementById("multcost").innerHTML = multicost;
             }
 
 
